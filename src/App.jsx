6eeb81f47644d92ea1ad3738e40f0b34d63c4fbf
“dennohkim” <kimathidennis38@gmail.com>
2022-12-13 22:46:42 +0300

    chore: add tailwind directives

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <div className="App">
-      <p className="font-bold">
+      <p className="font-bold text-4xl">
         Click on the Vite and React logos to learn more
       </p>
     </div>
