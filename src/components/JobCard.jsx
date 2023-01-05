402f662be8df5727009e9a7093224340fe960403
“dennohkim” <kimathidennis38@gmail.com>
2023-01-05 19:36:38 +0300

    feat: create job card

--- src/components/JobCard.jsx
+++ src/components/JobCard.jsx
@@ -1,15 +1,35 @@
-import React from 'react';
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
-const JobCard = ({jobId, company_logoUrl, company_name, role, role_time, location, uploaded_time }) => {
+const JobCard = ({
+  jobId,
+  company_logoUrl,
+  company_name,
+  role,
+  role_time,
+  location,
+  uploaded_time,
+}) => {
   return (
     <Link to={`/job/${jobId}`}>
-        <div className='rounded-md shadow-md'>
-
+      <div className="rounded-md shadow-md flex justify-between mb-8">
+        <div className="flex space-x-3">
+          <div>
+            <img src={company_logoUrl} alt="" />
+          </div>
+          <div className="flex flex-col space-y-3">
+            <p>{company_name}</p>
+            <p>{role}</p>
+            <button>{role_time}</button>
+          </div>
         </div>
+        <div className="flex justify-between">
+          <p>{location}</p>
+          <p>{uploaded_time}</p>
+        </div>
+      </div>
     </Link>
-    
-  )
-}
+  );
+};
 
-export default JobCard
+export default JobCard;
